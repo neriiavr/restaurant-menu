beb792cf61683671c44ddf6e12c9bbec99a03653
neriiavr <avrupalineriman@gmail.com>
2023-01-02 17:52:27 +0100

    implementation of css for the laurels

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
 
+import { SubHeading } from "../../components"
+import { images } from "../../constants"
 import './Gallery.css';
 
 const Gallery = () => (
